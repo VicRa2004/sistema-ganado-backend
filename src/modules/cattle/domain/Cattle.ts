90c718cdb09eb88cabd4157f90c3e57f844af58c
Victor Raúl García García <victor07mejor@gmail.com>
2025-11-18 22:35:28 -0600

    feat: Avana el dominio de cattle con getters y setters

--- src/modules/cattle/domain/Cattle.ts
+++ src/modules/cattle/domain/Cattle.ts
@@ -7,12 +7,32 @@ import { LotNumber } from "./value-object/LotNumber";
 import { Status } from "./value-object/Status";
 import { Gender } from "./Gender";
 
+interface CattleProps {
+  id?: number;
+  description: string;
+  gender: Gender;
+  registrationNumber: string;
+  lotNumber: string;
+  color: string;
+  birthdate: Date;
+  observations: string;
+  image?: string;
+  reasonForWithdrawal?: string;
+  status: boolean;
+  idFather?: number;
+  idMother?: number;
+  idIron: number;
+  idRace: number;
+  idUser: number;
+  idGround: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
 export class Cattle extends AggregateRoot {
   private constructor(
     id: Id,
     private descripcion: StringValue,
-    private idFather: Id,
-    private idMother: Id,
     private gender: Gender,
     private registrationNumber: RegistrationNumber,
     private lotNumber: LotNumber,
@@ -22,6 +42,8 @@ export class Cattle extends AggregateRoot {
     private image: StringValue,
     private reasonForWithdrawal: StringValue,
     private status: Status,
+    private idFather: Id,
+    private idMother: Id,
     private idIron: Id,
     private idRace: Id,
     private idUser: Id,
@@ -32,11 +54,130 @@ export class Cattle extends AggregateRoot {
     super(id, createdAt, updatedAt);
   }
 
+  static create(props: CattleProps) {}
+
   getDescripcion() {
     this.descripcion.getValue();
   }
 
+  getGender() {
+    return this.gender;
+  }
+
+  getRegistrationNumber() {
+    return this.registrationNumber.getValue();
+  }
+
+  getLotNumber() {
+    return this.lotNumber.getValue();
+  }
+
+  getColor() {
+    return this.color.getValue();
+  }
+
+  getBirthdate() {
+    return this.birthdate.getValue();
+  }
+
+  getObservations() {
+    return this.observations.getValue();
+  }
+
+  getImage() {
+    return this.image.getRawValue();
+  }
+
+  getReasonForWithdrawal() {
+    return this.reasonForWithdrawal.getRawValue();
+  }
+
+  getStatus() {
+    return this.status.getValue() === 1;
+  }
+
+  getIdFather() {
+    return this.idFather.getRawValue();
+  }
+
+  getIdMother() {
+    return this.idMother.getRawValue();
+  }
+
+  getIdIron() {
+    return this.idIron.getValue();
+  }
+
+  getIdRace() {
+    return this.idRace.getValue();
+  }
+
+  getIdUser() {
+    return this.idUser.getValue();
+  }
+
+  getIdGround() {
+    return this.idGround.getRawValue();
+  }
+
   setDescripcion(descripcion: string) {
     this.descripcion = StringValue.create(descripcion);
+  }
+
+  setGender(gender: Gender) {
+    this.gender = gender;
+  }
+
+  setRegistrationNumber(registrationNumber: string) {
+    this.registrationNumber = RegistrationNumber.create(registrationNumber);
+  }
+
+  setLotNumber(lotNumber: string) {
+    this.lotNumber = LotNumber.create(lotNumber);
+  }
+
+  setColor(color: string) {
+    this.color = StringValue.create(color);
+  }
+
+  setBirthdate(birthdate: Date) {
+    this.birthdate = DateValue.create(birthdate);
+  }
+
+  setObservations(observations: string) {
+    this.observations = StringValue.create(observations);
+  }
+
+  setImage(image: string) {
+    this.image = StringValue.create(image);
+  }
+
+  setReasonForWithdrawal(value: string) {
+    this.reasonForWithdrawal = StringValue.create(value);
+  }
+
+  setStatus(status: boolean) {
+    const value = status ? 1 : 0;
+    this.status = Status.create(value);
+  }
+
+  setIdFather(idFather: number) {
+    this.idFather = Id.create(idFather);
+  }
+
+  setIdMother(idMother: number) {
+    this.idMother = Id.create(idMother);
+  }
+
+  setIdIron(idIron: number) {
+    this.idIron = Id.create(idIron);
+  }
+
+  setIdRace(idRace: number) {
+    this.idRace = Id.create(idRace);
+  }
+
+  setIdGround(idGround: number) {
+    this.idGround = Id.create(idGround);
   }
 }
