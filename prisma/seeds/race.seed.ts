af01b0744aba0e8dee62bab6deab0506dafe2db4
Victor Raúl García García <victor07mejor@gmail.com>
2025-11-23 19:22:58 -0600

    feat: agrega lo necesario para hacer un seed de races

---
+++ prisma/seeds/race.seed.ts
@@ -0,0 +1,8 @@
+import { container } from "../../src/core/container/";
+import { RaceCreateDTO } from "../../src/modules/race/application/dtos/RaceCreateDTO";
+
+const items: RaceCreateDTO[] = [];
+
+export const seedRace = async () => {
+  items.forEach(async (item) => {});
+};
